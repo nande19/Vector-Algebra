fff653129cc4ff24add88b6167c5097cc23d917d
ST10082757 <ST10082757@vcconnect.edu.za>
2024-08-10 17:42:28 +0200

    Add project files.

---
+++ VectorAlgebra/Arrays.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VectorAlgebra
+{
+    public class Arrays
+    {
+        //adds corresponding elements of two 3D vectors and returns the results
+        public static int[,,] AddVector3D(int[,,] vector1, int[,,] vector2)
+        {
+            // Check if the vectors are of the same size
+            if (vector1.GetLength(0) != vector2.GetLength(0) ||
+                vector1.GetLength(1) != vector2.GetLength(1) ||
+                vector1.GetLength(2) != vector2.GetLength(2))
+            {
+                throw new ArgumentException("Vectors must be of the same size.");
+            }
+
+            int x = vector1.GetLength(0);
+            int y = vector1.GetLength(1);
+            int z = vector1.GetLength(2);
+            int[,,] result = new int[x, y, z];
+
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    for (int k = 0; k < z; k++)
+                    {
+                        result[i, j, k] = vector1[i, j, k] + vector2[i, j, k];
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        //subtracts corresponding elements of two 3D vectors and returns the resulting vector
+        public static int[,,] SubtractVector3D(int[,,] vector1, int[,,] vector2)
+        {
+            // Check if the vectors are of the same size
+            if (vector1.GetLength(0) != vector2.GetLength(0) ||
+                vector1.GetLength(1) != vector2.GetLength(1) ||
+                vector1.GetLength(2) != vector2.GetLength(2))
+            {
+                throw new ArgumentException("Vectors must be of the same size.");
+            }
+
+            int x = vector1.GetLength(0);
+            int y = vector1.GetLength(1);
+            int z = vector1.GetLength(2);
+            int[,,] result = new int[x, y, z];
+
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    for (int k = 0; k < z; k++)
+                    {
+                        result[i, j, k] = vector1[i, j, k] - vector2[i, j, k];
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        //multiplies each element of a 3D vector by a scalar and returns the resulting vector   
+        public static int[,,] ScalarMultiplyVector3D(int[,,] vector, int multiplier)
+        {
+            int x = vector.GetLength(0);
+            int y = vector.GetLength(1);
+            int z = vector.GetLength(2);
+            int[,,] result = new int[x, y, z];
+
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    for (int k = 0; k < z; k++)
+                    {
+                        result[i, j, k] = vector[i, j, k] * multiplier;
+                    }
+                }
+            }
+
+            return result;
+        }
+
+
+    }
+}
---
+++ VectorAlgebra/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VectorAlgebra
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //2D arrays 
+            int[,,] vector1 = { { { 1, 2 }, { 3, 4 } }, { { 5, 6 }, { 7, 8 } } };
+            int[,,] vector2 = { { { 1, 1 }, { 1, 1 } }, { { 1, 1 }, { 1, 1 } } };
+
+            //results are stored here 
+            var added = Arrays.AddVector3D(vector1, vector2);
+            var subtracted = Arrays.SubtractVector3D(vector1, vector2);
+            var multiplied = Arrays.ScalarMultiplyVector3D(vector1, 2);
+
+            //printing the results
+            Console.WriteLine("Addition Result:");
+            PrintVector(added);
+            Console.WriteLine("Subtraction Result:");
+            PrintVector(subtracted);
+            Console.WriteLine("Scalar Multiplication Result:");
+            PrintVector(multiplied);
+        }
+
+        //takes 3D integear array and prints it
+        static void PrintVector(int[,,] vector)
+        {
+            int x = vector.GetLength(0);
+            int y = vector.GetLength(1);
+            int z = vector.GetLength(2);
+
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    for (int k = 0; k < z; k++)
+                    {
+                        Console.Write(vector[i, j, k] + " ");
+                    }
+                    Console.WriteLine();
+                }
+                Console.WriteLine();
+            }
+        }
+       
+    }
+}
